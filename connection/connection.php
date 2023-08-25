c15f79eb1f84b3b88c62da97227de6c0c59421ae
mois3222 <moisky222@gmail.com>
2023-08-25 18:47:40 -0500

    form done

--- connection/connection.php
+++ connection/connection.php
@@ -1,4 +1,22 @@
 <?php
-    class Cconnection{
-        
+class Cconnection
+{
+    public static function connectionBD()
+    {
+        $localhost = "localhost";
+        $dbname = "loginphp";
+        $user = "root";
+        $password = "123456";
+
+        try {
+            $connection = new PDO("mysql:host=$localhost; dbname=$dbname;user=$user; password=$password");
+            echo "Connneted....";
+            $statement = $connection->prepare("SELECT * FROM login_table");
+            $statement->execute();
+            $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+            print_r($records);
+        } catch (PDOException $error) {
+            echo $error->getMessage();
+        }
     }
+}
--- index.php
+++ index.php
@@ -4,11 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
     <title>php connection with MySQL server</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            min-height: 100vh;
+            font-family: 'Inter', sans-serif;
+        }
+
+        main {
+            max-width: 90vw;
+        }
+    </style>
 </head>
 
 <body>
-
+    <main>
+        <form action="login/login.php">
+            <h1>Sing in</h1>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Name</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </main>
 </body>
 
 </html>
